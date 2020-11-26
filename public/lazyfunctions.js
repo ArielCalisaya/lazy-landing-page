3f3c6ca03f9df94f94e58128fcf963cf70a121a4
CSAV <momonkov@localhost.localdomain>
2020-11-26 17:40:55 -0300

    deployed to firebase

---
+++ public/lazyfunctions.js
@@ -0,0 +1,29 @@
+
+const navbarButton = document.querySelector('#nav-mobile')
+const navLinks = document.querySelector('.nav-links')
+const links = document.querySelectorAll('.nav-links li')
+
+navbarButton.addEventListener("click", () => {
+    navLinks.classList.toggle('open')
+})
+
+const textbutton = document.querySelector('.togglerText')
+const world = document.querySelector('.world')
+const kittenWhoa = document.querySelector('.kitten-whoa')
+
+textbutton.addEventListener("click", () => {
+    textbutton.classList.toggle('toggled')
+    world.classList.toggle('toggle')
+    kittenWhoa.classList.toggle('toggle')
+}) 
+
+world.addEventListener("click", () => {
+    world.classList.toggle('toggle')
+    kittenWhoa.classList.toggle('toggle')
+    textbutton.classList.toggle('toggled')
+}) 
+kittenWhoa.addEventListener("click", () => {
+    world.classList.toggle('toggle')
+    kittenWhoa.classList.toggle('toggle')
+    textbutton.classList.toggle('toggled')
+}) 
