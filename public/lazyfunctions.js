5009429299e40ffcd5894388be1890e2acf61b07
CSAV <momonkov@localhost.localdomain>
2020-11-26 18:03:10 -0300

    doc formated

--- public/lazyfunctions.js
+++ public/lazyfunctions.js
@@ -1,29 +1,28 @@
-
-const navbarButton = document.querySelector('#nav-mobile')
-const navLinks = document.querySelector('.nav-links')
-const links = document.querySelectorAll('.nav-links li')
+const navbarButton = document.querySelector("#nav-mobile");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
 
 navbarButton.addEventListener("click", () => {
-    navLinks.classList.toggle('open')
-})
+  navLinks.classList.toggle("open");
+});
 
-const textbutton = document.querySelector('.togglerText')
-const world = document.querySelector('.world')
-const kittenWhoa = document.querySelector('.kitten-whoa')
+const textbutton = document.querySelector(".togglerText");
+const world = document.querySelector(".world");
+const kittenWhoa = document.querySelector(".kitten-whoa");
 
 textbutton.addEventListener("click", () => {
-    textbutton.classList.toggle('toggled')
-    world.classList.toggle('toggle')
-    kittenWhoa.classList.toggle('toggle')
-}) 
+  textbutton.classList.toggle("toggled");
+  world.classList.toggle("toggle");
+  kittenWhoa.classList.toggle("toggle");
+});
 
 world.addEventListener("click", () => {
-    world.classList.toggle('toggle')
-    kittenWhoa.classList.toggle('toggle')
-    textbutton.classList.toggle('toggled')
-}) 
+  world.classList.toggle("toggle");
+  kittenWhoa.classList.toggle("toggle");
+  textbutton.classList.toggle("toggled");
+});
 kittenWhoa.addEventListener("click", () => {
-    world.classList.toggle('toggle')
-    kittenWhoa.classList.toggle('toggle')
-    textbutton.classList.toggle('toggled')
-}) 
+  world.classList.toggle("toggle");
+  kittenWhoa.classList.toggle("toggle");
+  textbutton.classList.toggle("toggled");
+});
